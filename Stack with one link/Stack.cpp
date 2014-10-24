25d993bbd9d112b1c6d8f6f874f3c6a4fb49909d
Anton94 <antonvdudov@gmail.com>
2014-10-24 22:50:19 +0300

    Added operator=

--- Stack with one link/Stack.cpp
+++ Stack with one link/Stack.cpp
@@ -6,35 +6,40 @@
 
 int main()
 {
-	//		lHighWaterCount	3435973836	unsigned int
-
 	try
 	{
-		_CrtMemState s1;
+		_CrtMemState s1,s2,s3;
 		_CrtMemCheckpoint(&s1);
+		std::cout << "One linked stack (next element)\n";
+		Stack<int> stack;
+		for (int i = 0; i < 6; ++i)
 		{
-			std::cout << "One linked stack (next element)\n";
-			Stack<int> stack;
-			for (int i = 0; i < 12; ++i)
-			{
-				stack.push(i * 11);
-			}
-
-			Stack<int> stackCopy(stack);
-
-			for (int i = 0; i < 12; ++i)
-			{
-				std::cout << "Peek from stack -> " << stack.peek() << std::endl;
-				stack.pop();
-			}
-
-			for (int i = 0; i < 12; ++i)
-			{
-				std::cout << "Peek from stackCopy -> " << stackCopy.peek() << std::endl;
-				stackCopy.pop();
-			}
+			stack.push(i * 11);
 		}
-		_CrtMemDumpStatistics(&s1);
+
+		Stack<int> stackCopy;
+		stackCopy = stack;
+
+		while (!stack.isEmpty())
+		{
+			std::cout << "Peek from stack -> " << stack.peek() << std::endl;
+			stack.pop();
+		}
+
+		while (!stackCopy.isEmpty())
+		{
+			std::cout << "Peek from stackCopy -> " << stackCopy.peek() << std::endl;
+			stackCopy.pop();
+		}		
+
+		_CrtMemCheckpoint(&s2);
+
+		if (_CrtMemDifference(&s3, &s1, &s2))
+		{
+			std::cout << "Memory leaks...\n";
+			_CrtMemDumpStatistics(&s3);
+		}
+			
 	}
 	catch (const char* msg)
 	{
--- Stack with one link/Stack.h
+++ Stack with one link/Stack.h
@@ -7,90 +7,175 @@
 template <class T>
 struct elem
 {
+public:
+	elem(const T& x, elem<T>* p = NULL);
+	elem(const elem & other);
+	elem& operator=(const elem& other);
+	~elem();
+private:
+	void copyFrom(const elem & other);
+	void free();
+public:
 	T data;
 	elem<T>* next;
-	elem(const T& x, elem<T>* p = NULL) : data(x), next(p) {}
-	elem(const elem & other)
-	{
-		
-		data = other.data;
-		if (other.next)
-		{
-			next = new elem<T>(*other.next);
-		}
-		else
-		{
-			next = NULL;
-		}
-	}
-	~elem()
-	{
-		if (next)
-		{
-			delete next;
-		}
-	}
 };
+
+template <class T>
+elem<T>::elem(const T& x, elem<T>* p = NULL) : data(x), next(p) {}
+
+template <class T>
+elem<T>::elem(const elem & other)
+{
+	copyFrom(other);
+}
+
+template <class T>
+elem<T>& elem<T>::operator=(const elem & other)
+{
+	if (this != &other)
+	{
+		free();
+		copyFrom(other);
+	}
+
+	return *this;
+}
+
+template <class T>
+elem<T>::~elem()
+{
+	free();
+}
+
+template <class T>
+void elem<T>::copyFrom(const elem & other)
+{
+	data = other.data;
+
+	if (other.next)
+	{
+		next = new elem<T>(*other.next);
+	}
+	else
+	{
+		next = NULL;
+	}
+}
+
+template <class T>
+void elem<T>::free()
+{
+	if (next)		  // if next is NULL it will do , anyway..
+		delete next;  // deletes the memory and calls the destructor of the element, pointed by the 'next' and etc...
+}
+
 
 template <class T>
 class Stack
 {
 public:
-	Stack() : tos(NULL) {}
-	Stack(const Stack& other)
-	{
-		if (other.tos)
-			tos = new elem<T>(*other.tos);
-	}
+	Stack();
+	Stack(const Stack& other);
+	Stack& operator=(const Stack& other);
+	~Stack();
+public:
+	const T& peek() const;
+	T pop();
+	void push(const T& val);
+	bool isEmpty() const;
+private:
+	void free();
+	void copyFrom(const Stack& other);
 
-	const T& peek() const
-	{
-		if (isEmpty())
-		{
-			throw "Stack is empty and called peek!";
-		}
-
-		return tos->data;
-	}
-
-	T pop()
-	{
-		if (isEmpty())
-		{
-			throw "Stack is empty and called pop!";
-		}
-
-		T val = tos->data;
-		elem<T> * temp = tos;
-		tos = tos->next;
-		temp->next = NULL; // break the cascade destructor
-		delete temp;
-		return val;
-	}
-
-	void push(const T& val)
-	{
-		elem<T> * temp = new elem < T >(val);
-		temp->next = tos;
-		tos = temp;
-
-		// OR:  elem<T> * tos = new elem<T>(val, tos);
-	}
-
-	bool isEmpty() const
-	{
-		return tos == NULL; // !tos
-	}
-
-	~Stack()
-	{
-		delete tos;
-	}
 private:
 	elem<T> * tos; // top of stack
-
-
 };
+
+template <class T>
+Stack<T>::Stack() : tos(NULL) {}
+
+template <class T>
+Stack<T>::Stack(const Stack& other)
+{
+	copyFrom(other);
+}
+
+template <class T>
+Stack<T>& Stack<T>::operator=(const Stack& other)
+{
+	if (this != &other)
+	{
+		free();
+		copyFrom(other);
+	}
+
+	return *this;
+}
+
+template <class T>
+Stack<T>::~Stack()
+{
+	delete tos;
+}
+
+template <class T>
+const T& Stack<T>::peek() const
+{
+	if (isEmpty())
+	{
+		throw "Stack is empty and called peek!";
+	}
+
+	return tos->data;
+}
+
+template <class T>
+T Stack<T>::pop()
+{
+	if (isEmpty())
+	{
+		throw "Stack is empty and called pop!";
+	}
+
+	T val = tos->data;
+	elem<T> * temp = tos;
+	tos = tos->next;
+	temp->next = NULL; // break the cascade destructor
+	delete temp;
+	return val;
+}
+
+template <class T>
+void Stack<T>::push(const T& val)
+{
+	elem<T> * temp = new elem < T >(val);
+	temp->next = tos;
+	tos = temp;
+
+	// OR:  elem<T> * tos = new elem<T>(val, tos);
+}
+
+template <class T>
+bool Stack<T>::isEmpty() const
+{
+	return tos == NULL; // !tos
+}
+
+template <class T>
+void  Stack<T>::free()
+{
+	while (!isEmpty())
+	{
+		pop();
+	}
+}
+
+template <class T>
+void  Stack<T>::copyFrom(const Stack& other)
+{
+	if (other.tos)
+		tos = new elem<T>(*other.tos);
+}
 
 #endif
 
