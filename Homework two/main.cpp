d2c94fc82ee6b55cc9d7a037d75996c34d2a73ce
Anton94 <antonvdudov@gmail.com>
2014-11-13 21:41:37 +0200

    Startet second homework for SD course

---
+++ Homework two/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "../Double linked list/DLList.h"
+#include "../Queue with one link/Queue.h"
+
+int main()
+{
+	std::cout << "Here we starsts..." << std::endl;
+	return 0;
+}
