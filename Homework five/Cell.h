ae18d91490b3d894d1292097c5a368306bea9ef6
Anton94 <antonvdudov@gmail.com>
2015-01-20 18:57:45 +0200

    Update

--- Homework five/Cell.h
+++ Homework five/Cell.h
@@ -2,12 +2,28 @@
 
 class Cell;
 
-#include "Map.h"
+#include "StreetMap.h"
+
+/*
+	Default copy constructor and default operator= 
+
+	pourOut : get the water from the last pour and pours the water to the toAdd variable on the neighbours cells. 
+	Because of that it had to make last iteration on the map to update all cells. 
+*/
 
 class Cell
 {
+	friend class StreetMap;
+public:
+	Cell(int indexRow, int indexCol, double height, StreetMap * owner);
+	void setWater(double water);
+	bool pourOut();
+	void updateAll(); // adds toAdd value to the water value.
 private:
 	int indexRow;
 	int indexCol;
-	Map** map;
+	double height;
+	StreetMap* owner;
+	double water;
+	double toAdd;
 };
--- Homework five/StreetMap.h
+++ Homework five/StreetMap.h
@@ -1,15 +1,15 @@
 #pragma once
 
 // have declaration to make posible map in cell, and cell in map , some fucked up shits...
-class Map;
+class StreetMap;
 
 #include "Cell.h"
 
 
-
-
-class Map
+class StreetMap
 {
+public:
+
 private:
 	Cell ** map;
 };
