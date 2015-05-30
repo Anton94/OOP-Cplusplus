6987d372bf49056066bdebed9632da7d2f762cd4
Anton94 <antonvdudov@gmail.com>
2015-05-30 12:14:18 +0300

    Suffix array with working bounds, but RIGHT one is slow

--- Suffix Array/Suffix Array/Source.cpp
+++ Suffix Array/Suffix Array/Source.cpp
@@ -2,14 +2,94 @@
 #include <string>
 #include "SuffixArray.h"
 
+using std::string;
+
+int arr[12] = { 1, 2, 3, 4, 5, 5, 5, 6, 6, 7, 8, 9};
+
+// A Binary search algorithm for lower bound.
+int lowerBoundOfSuffixesWithPattern(int x)
+{
+	int low = 0;
+	int high = sizeof(arr)/sizeof(int) - 1;
+	int mid = 0;
+
+	while (low < high)
+	{
+		mid = (low + high) / 2;
+		if (x <= arr[mid]) // TO DO -> partial checking
+			high = mid;
+		else
+			low = mid + 1;
+	}
+
+	return low;
+}
+
+// A binary search algorithm for upper bound.
+int upperBoundOfSuffixesWithPattern(int x)
+{
+	int low = 0;
+	int high = sizeof(arr) / sizeof(int) - 1;
+	int mid = 0;
+
+	while (low < high)
+	{
+		mid = (low + high + 1) / 2;
+		if (arr[mid] <= x) // TO DO -> partial checking
+			low = mid;
+		else
+			high = mid - 1;
+	}
+
+	return low;
+}
+
+
 int main()
 {
-	string text;
-	cin >> text;
+	// Memory leaks cheching...
+	_CrtMemState s1, s2, s3;
+	_CrtMemCheckpoint(&s1);
+	{
+		string text;
+		//std::cin >> text;
 
-	SuffixArray sa(text.c_str());
+		text = "abcabdescaab";
 
-	sa.print(std::cout);
+		SuffixArray sa(text.c_str());
+
+		sa.print(std::cout);
+
+		sa.search("caa");
+
+		std::cout << "Lower bound of 5 -> " << lowerBoundOfSuffixesWithPattern(5) << std::endl;
+		std::cout << "Upper bound of 5 -> " << upperBoundOfSuffixesWithPattern(5) << std::endl;
+
+		std::cout << "Lower bound of 7 -> " << lowerBoundOfSuffixesWithPattern(7) << std::endl;
+		std::cout << "Upper bound of 7 -> " << upperBoundOfSuffixesWithPattern(7) << std::endl;
+
+		std::cout << "Lower bound of 8 -> " << lowerBoundOfSuffixesWithPattern(8) << std::endl;
+		std::cout << "Upper bound of 8 -> " << upperBoundOfSuffixesWithPattern(8) << std::endl;
+
+		std::cout << "Lower bound of 9 -> " << lowerBoundOfSuffixesWithPattern(9) << std::endl;
+		std::cout << "Upper bound of 9 -> " << upperBoundOfSuffixesWithPattern(9) << std::endl;
+
+		std::cout << "Lower bound of 165 -> " << lowerBoundOfSuffixesWithPattern(165) << std::endl;
+		std::cout << "Upper bound of 165 -> " << upperBoundOfSuffixesWithPattern(165) << std::endl;
+
+		std::cout << "Lower bound of -165 -> " << lowerBoundOfSuffixesWithPattern(-165) << std::endl;
+		std::cout << "Upper bound of -165 -> " << upperBoundOfSuffixesWithPattern(-165) << std::endl;
+
+		std::cout << "Compare aba and aba" << sa.compare("aba", "abac") << std::endl;
+	}
+
+	_CrtMemCheckpoint(&s2);
+
+	if (_CrtMemDifference(&s3, &s1, &s2))
+	{
+		std::cout << "Memory leak detected!" << std::endl;
+		_CrtMemDumpStatistics(&s3);
+	}
 
 	return 0;
 }
--- Suffix Array/Suffix Array/SuffixArray.h
+++ Suffix Array/Suffix Array/SuffixArray.h
@@ -1,14 +1,11 @@
-#pragma once
+﻿#pragma once
 
-#include <iostream>
-#include <cstring>
+#include <vector>
 #include <algorithm>
 
-using std::string;
-using std::cin;
-using std::cout;
+/// TO DO: start from 1 to length.
 
-
+using std::vector;
 
 class SuffixArray
 {
@@ -24,6 +21,7 @@ class SuffixArray
 public:
 	SuffixArray(const char* text)
 	{
+		// Default values(in any case. the first memory allocation may fail)
 		setDefaultValues();
 
 		// Get the length of the text.
@@ -34,7 +32,7 @@ public:
 		strcpy(this->text, text);
 
 		// Create the array with the start of every suffix (so from 0 to length-1)
-		suffixes = new unsigned[length + 1];
+		suffixes = new unsigned[length];
 		for (unsigned i = 0; i < length; ++i)
 			suffixes[i] = i;
 
@@ -42,12 +40,37 @@ public:
 		sortSuffixes();
 	}
 
+	// Returns a vector of start positions of the suffixes, which has prefix the given pattern.  
+	vector<int> search(const char* pattern) const
+	{
+		vector<int> res;
+
+		int lowerBound = lowerBoundOfSuffixesWithPattern(pattern),
+			   upperBound = upperBoundOfSuffixesWithPattern(pattern);
+
+		std::cout << "Lower bound is " << lowerBound << std::endl;
+		std::cout << "Upper bound is " << upperBound << std::endl;
+
+		return res;
+	}
+
+	// Prints all suffixes of the SuffixArray to the given ostream.
 	void print(std::ostream& out) const
 	{
 		out << "Suffixes: \n";
 		for (unsigned i = 0; i < length; ++i)
 		{
-			out << (text + suffixes[i]) << "\n";
+			out << i << " " << (text + suffixes[i]) << "\n";
+		}
+	}
+
+	// Prints all suffixes with start positions are in the given vector(the values in the vector, are the start positions of the suffixes) to the given ostream.
+	void printAllSuffixesWithStart(std::ostream& out, const vector<int>& v) const
+	{
+		size_t n = v.size();
+		for (size_t i = 0; i < n; ++i)
+		{
+			out << (text + suffixes[v[i]]) << "\n";
 		}
 	}
 
@@ -55,7 +78,85 @@ public:
 	{
 		free();
 	}
+
+	// Counts the number of same chars from the start of the two strings
+	unsigned compare(const char* s1, const char* s2) const
+	{
+		unsigned count = 0;
+		while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+		{
+			++count;
+			++s1;
+			++s2;
+		}
+
+		return count;
+	}
 private:
+
+
+	// A Binary search algorithm for lower bound.
+	int lowerBoundOfSuffixesWithPattern(const char* pattern) const
+	{
+		int low = 0;
+		int high = length - 1;
+		int mid = 0;
+		unsigned matchCount = 0;
+		unsigned patternLength = strlen(pattern);
+		
+		while (low < high)
+		{
+			mid = (low + high) / 2;
+			if (strcmp(pattern + matchCount, text + suffixes[mid] + matchCount) <= 0) // TO DO -> partial checking
+			{
+				high = mid;
+				matchCount += compare(text + suffixes[mid] + matchCount, text + suffixes[low] + matchCount);
+			}
+			else
+			{
+				low = mid + 1;
+				matchCount += compare(text + suffixes[mid + 1] + matchCount, text + suffixes[high] + matchCount);
+			}
+		}
+
+		return low;
+	}
+
+	// A binary search algorithm for upper bound.
+	int upperBoundOfSuffixesWithPattern(const char* pattern) const
+	{
+		int low = 0;
+		int high = length - 1;
+		int mid = 0;
+		unsigned matchCount = 0;
+		unsigned patternLength = strlen(pattern);
+
+		while (low < high)
+		{
+
+			mid = (low + high + 1) / 2;
+			//std::cout << "compares with middle = " << mid << " string: " << text + suffixes[mid] << " and string: " << pattern << std::endl;
+			//std::cout << "matchCount = " << matchCount << std::endl;
+			if (strstr(text + suffixes[mid], pattern) == text + suffixes[mid] || strcmp(text + suffixes[mid], pattern) <= 0) // TO DO -> partial checking
+			{
+			//	matchCount += compare(text + suffixes[mid] + matchCount, text + suffixes[high] + matchCount);
+			//	std::cout << "matchCount = " << matchCount << std::endl;
+			//	std::cout << "true" << std::endl;
+				low = mid;
+			}
+			else
+			{
+			//	matchCount += compare(text + suffixes[mid] + matchCount, text + suffixes[low] + matchCount); // Mid - 1 == 0?
+			//	std::cout << "matchCount = " << matchCount << std::endl;
+
+				high = mid - 1;
+			//	std::cout << "false" << std::endl;
+			}
+		}
+
+		return low;
+	}
+
 	// Sets the default values for the object member datas.
 	void setDefaultValues()
 	{
