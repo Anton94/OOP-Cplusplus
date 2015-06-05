908a3067d12f5c88967d2aad8322047c8aebf4ac
Anton94 <antonvdudov@gmail.com>
2015-06-05 14:21:42 +0300

    Finished append functionality

--- Persistent vector/Persistent vector/PersistentVector.h
+++ Persistent vector/Persistent vector/PersistentVector.h
@@ -5,8 +5,8 @@
 	* #71488
 	* Github repository: 
 	*
-	*
-	*
+	* The starting level of the vector tree is 6.
+	* Optimization maybe I can keep the position of the rightmost taken node in child node vector.
 	*
 	*
 */
@@ -14,8 +14,9 @@
 #include <type_traits>
 #include <vector>
 
-// 32 childs per root for the tree.
+// 32 childs per node for the tree.
 #define B 32
+#define MAX_LEVEL 5
 
 using std::vector;
 
@@ -28,13 +29,13 @@ private:
 
 	/*
 		Nodes.
-		I use the default copy constructor and assignment operator.
+		
 	*/
 
 	// Basic abstract class.
 	struct Node
 	{
-		virtual ~Node() {}; // future stuff for deletion, maybe.
+		virtual ~Node() = 0 {}; // future stuff for deletion, maybe. Pure virtual with a body, because the derive classes will call the base class destructor.
 	};
 
 	// Internal node.
@@ -48,6 +49,11 @@ private:
 			childs.resize(B, NULL);
 		}
 
+		NodeInternal(const vector<Node*>& ch)
+		{
+			childs = ch;
+		}
+
 		virtual ~NodeInternal() {}; // future stuff for deletion, maybe.
 	};
 
@@ -56,9 +62,11 @@ private:
 	{
 		vector<T> values;
 
-		NodeLeaf()
+		NodeLeaf() {}
+
+		NodeLeaf(const vector<T>& v)
 		{
-			values.resize(B); // The default values doesn`t matter so much...
+			values = v;
 		}
 
 		virtual ~NodeLeaf() {}; // future stuff for deletion, maybe.
@@ -75,7 +83,7 @@ private:
 public:
 	PersistentVector()
 	{
-		root = new NodeInternal();
+		buildPathToTheLeaf(MAX_LEVEL, root); // From level 5(real root) to level 0(the leaf level).
 		count = 0;
 	}
 
@@ -86,7 +94,28 @@ public:
 	PersistentVector update(int index, T value) const;
 
 	// Returns a new vector with *value* appended at the end.
-	PersistentVector append(T value) const;
+	PersistentVector append(T value) const
+	{
+		// Creates exact copy of the vector.
+		PersistentVector<T> newVector(*this);
+
+
+		// If onle need to append the value in the vector of valus in the leaf node.
+		if (checkForRoomInRightmostLeafNode(MAX_LEVEL, root))
+		{
+			appendRightmostPath(MAX_LEVEL, newVector.root, value);
+		}
+		else
+		// If needed creates new internal nodes and adds the value.
+		{
+			append(MAX_LEVEL, newVector.root, value);
+		}
+
+
+		++(newVector.count);
+
+		return newVector;
+	}
 
 	// Returns a new vector that's the same as this one but without the last element.
 	PersistentVector pop() const;
@@ -94,5 +123,170 @@ public:
 	int size() const
 	{
 		return count;
+	}
+
+
+	// Basic DFS prints only leaf values.
+	void print(std::ostream& out) const
+	{
+		print(MAX_LEVEL, out, root);
+		out << "\n";
+	}
+private:
+	// Basic DFS prints only leaf values.
+	void print(int level, std::ostream& out, Node * root) const
+	{
+		if (!root)
+			return;
+		if (level > 0)
+		{
+			NodeInternal * temp = dynamic_cast<NodeInternal*>(root);
+			for (int i = 0; i < B; ++i)
+			{
+				print(level - 1, out, temp->childs[i]);
+			}
+		}
+		else
+		{
+			NodeLeaf * temp = dynamic_cast<NodeLeaf*>(root);
+			if (!temp)
+				return;
+
+			for (int i = 0; i < temp->values.size(); ++i)
+			{
+				out << temp->values[i] << " ";
+			}
+		}
+	}
+
+	// Adds only the value in the new vector. (there is space in the rightmost leaf node)
+	void appendRightmostPath(int level, Node *& root, const T& value) const
+	{
+		if (level > 0)
+		{
+			// Copies the internal node.
+			root = new NodeInternal(dynamic_cast<NodeInternal*>(root)->childs);
+
+			// Goes to the rightmost path.
+			NodeInternal * temp = dynamic_cast<NodeInternal*>(root);
+
+			for (int i = 1; i < B; ++i)
+			{
+				if (temp->childs[i] == NULL)
+				{
+					appendRightmostPath(level - 1, temp->childs[i - 1], value);
+					return;
+				}
+			}
+		}
+		else
+		{
+			root = new NodeLeaf(dynamic_cast<NodeLeaf*>(root)->values);
+			dynamic_cast<NodeLeaf*>(root)->values.push_back(value);
+		}
+	}
+
+	// The rightmost leaf node is full, copies the path till it founds a full node (internal or leaf) and makes new path to the leaf level (from the level above the full node)
+	// and appends the value in the new leaf node.
+	void append(int level, Node *& root, const T& value) const
+	{
+		// There is no way to get in that case if the leaf node is full, but if so(other people use my gorgeous function), returns...
+		if (level <= 0)
+			return;
+
+		// Copies the internal node.
+		root = new NodeInternal(dynamic_cast<NodeInternal*>(root)->childs);
+
+		// Goes to the rightmost path.
+		NodeInternal * temp = dynamic_cast<NodeInternal*>(root);
+
+		// I know that the rightmost leaf is full, so I create new one.
+		if (level == 1)
+		{
+			for (int i = 1; i < B; ++i)
+			{
+				if (temp->childs[i] == NULL)
+				{
+					NodeLeaf* newLeaf = new NodeLeaf();
+					temp->childs[i] = newLeaf;
+					newLeaf->values.push_back(value);
+					return;					
+				}
+			}
+		}
+
+		// If every node is taken and the rightmost child node is full, then the vector is FULL. I can make that check with some range checking for the size...
+		for (int i = 1; i < B; ++i)
+		{
+			if (temp->childs[i] == NULL)
+			{
+				// if the rightmost child node is full, build new path and append it to the 'i-th' position.
+				if (getSizeOfNotNullPointers(((NodeInternal*)(temp->childs[i - 1]))->childs) >= B)// gets the real size of marked nodes.
+				{
+					buildPathToTheLeaf(level - 1, temp->childs[i])->values.push_back(value); // The function will return a pointer to the leaf node.
+				}
+				// Otherwise there is place in the rightmost child node, so go down.
+				else
+				{
+					append(level - 1, temp->childs[i - 1], value);
+				}
+
+				return;
+			}
+		}
+
+
+	}
+
+	//Till fisrt null ptr.
+	size_t getSizeOfNotNullPointers(const vector<Node*> & v) const
+	{
+		for (size_t i = 0; i < v.size(); ++i)
+		{
+			if (v[i] == NULL)
+				return i;
+		}
+	}
+
+	// From the starting level @startLevel to the leaf level. Leaf level is 0.
+	// Returns the leaf node.
+	NodeLeaf* buildPathToTheLeaf(int level, Node *& root) const // basicaly it`s not very const
+	{
+		if (level > 0)
+		{
+			NodeInternal * newNode = new NodeInternal();
+			root = newNode;
+			buildPathToTheLeaf(level - 1, newNode->childs[0]);
+		}
+		else
+		{
+			NodeLeaf * newNode = new NodeLeaf();
+			root = newNode;
+			return newNode;
+		}
+	}
+
+	// Checks if there is room in the rightmost leaf node. If so returns true.
+	bool checkForRoomInRightmostLeafNode(int level, Node * root) const
+	{
+		if (!root)
+			return false;
+		if (level <= 0)
+		{
+			return (dynamic_cast<NodeLeaf*>(root))->values.size() < B;
+		}
+		else
+		{
+			NodeInternal * temp = dynamic_cast<NodeInternal*>(root);
+			for (int i = 1; i < B; ++i)
+			{
+				if (temp->childs[i] == NULL)
+				{
+					return checkForRoomInRightmostLeafNode(level - 1, temp->childs[i - 1]);
+				}
+			}
+
+			return false;
+		}
 	}
 };
--- Persistent vector/Persistent vector/Source.cpp
+++ Persistent vector/Persistent vector/Source.cpp
@@ -4,7 +4,26 @@
 
 int main()
 {
-	PersistentVector<int> pv;
+	PersistentVector<int> pv1;
+	PersistentVector<int> pv2 = pv1.append(5);
+	PersistentVector<int> pv3 = pv1.append(15);
+	PersistentVector<int> pv4 = pv3.append(35);
+	pv1.print(std::cout);
+	pv2.print(std::cout);
+	pv3.print(std::cout);
+	pv4.print(std::cout);
+
+	const int COUNT = 32*32*32;
+
+	// NOTE the first vector is empty one. so valus from 0 to count - 2 (count - 1 but the first one is empty)
+	PersistentVector<int> arr[COUNT];
+
+	for (int i = 0; i < COUNT - 1; ++i)
+	{
+		arr[i + 1] = arr[i].append(i);
+	}
+
+	arr[COUNT - 1].print(std::cout);
 
 	return 0;
 }
