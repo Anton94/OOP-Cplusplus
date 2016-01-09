c1d7b0872eedd1f2683d140fa77b66f757342a58
Anton94 <antonvdudov@gmail.com>
2016-01-09 17:02:45 +0200

    Added test for the examples from the task PDF and memory check

--- Tree Isomorphism/Tree Isomorphism/main.cpp
+++ Tree Isomorphism/Tree Isomorphism/main.cpp
@@ -13,25 +13,48 @@
 #include <iostream>
 #include "Tree.h"
 
-int main()
+void testFromTheTaskExamples()
 {
+	// From the taks file examples:
 	Tree tree1("(5 {(9 {}) (1 {(4 {}) (12 {}) (42 {})})})");
 	//tree1.printDFS(std::cout);
 	tree1.printBFS(std::cout);
 	std::cout << std::endl;
 
-	Tree tree2("(7{(15 {(7 {}) (10 {})}) (3 {(8 {})})})");
+	Tree tree2("(7{(15 {(7 {}) (10 {}) (8 {})}) (3 {})})");
 	//tree2.printDFS(std::cout);
 	tree2.printBFS(std::cout);
 	std::cout << std::endl;
 
-	Tree tree3("(5 {(1 {(4 {}) (12 {}) (42 {})}) (9 {})})");
+	Tree tree3("(5 {(9 {}) (1 {(4 {}) (12 {}) (42 {})})})");
 	//tree3.printDFS(std::cout);
 	tree3.printBFS(std::cout);
 	std::cout << std::endl;
 
-	std::cout << "Isomorphism tree 1 and tree 2 (expects 0)? " << (tree1.isIsomorphWithOtherTree(tree2)) << std::endl;
-	std::cout << "Isomorphism tree 1 and tree 3 (expects 1)? " << (tree1.isIsomorphWithOtherTree(tree3)) << std::endl;
-	
+	Tree tree4("(7{(15 {(7 {}) (10 {})}) (3 {(8 {})})})");
+	//tree4.printDFS(std::cout);
+	tree4.printBFS(std::cout);
+	std::cout << std::endl;
+
+
+	std::cout << "Isomorphism tree 1 and tree 2 (expects 1)? " << (tree1.isIsomorphWithOtherTree(tree2)) << std::endl;
+	std::cout << "Isomorphism tree 3 and tree 4 (expects 0)? " << (tree3.isIsomorphWithOtherTree(tree4)) << std::endl;
+}
+
+int main()
+{
+	// Checks for memory leaks. Works only in VS (for example) the memory check.
+	_CrtMemState s1, s2, s3;
+	_CrtMemCheckpoint(&s1);
+	{
+		testFromTheTaskExamples();
+	}
+
+	_CrtMemCheckpoint(&s2);
+	if (_CrtMemDifference(&s3, &s1, &s2))
+	{
+		std::cout << "Memory leak detected!" << std::endl;
+		_CrtMemDumpStatistics(&s3);
+	}
 	return 0;
 }
